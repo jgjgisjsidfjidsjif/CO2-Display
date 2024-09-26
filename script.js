20ed677763337554cc19cd8207256a877068c2d1
jgjgisjsidfjidsjif <126114030+jgjgisjsidfjidsjif@users.noreply.github.com>
2024-09-26 19:19:17 +1200

    V2
    Improved version with Graphs and other improvements suggested by the public.

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
+//This little tidbit of code will activate the script and set it to automatically activate every 10 seconds.
 window.onload = getData();
 setInterval(() => getData(), 10000);
 
 
-async function getData(){ //This retrives data from the WAQI API for Pakuranga.
+let aqiChart;
+let pm10Chart;
+
+
+async function getData() { //This retrives data from the WAQI API for Pakuranga.
     const api = 'https://api.waqi.info/feed/@9305/?token=[REDACTED]';
     try {
         const response = await fetch(api);
-        if (!response.ok){
+        if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
         }
         const data = await response.json();
-        parseData(data);
-    }catch (error){
+        //This is where the magic happens. This if else statement determines which page the user is on, and determines which function should be used.
+        if (document.getElementById('extendedStatistics')){
+            parseData(data);
+            console.log("This is the index page.");
+        }
+        else{
+            parseExtended(data);
+            console.log("This is NOT!!! the index page.");
+        };
+         
+    } catch (error) {
         console.error(error.message);
     }
 }
 
-function parseData(data){
+//This function is called if the user is on the homepage. This function sets up and updates divs with the correct information.
+function parseData(data) {
     if (data.status === "ok") {
-        const { aqi, dominentpol, iaqi, city} = data.data;
-        const { t, h, p, pm10,} = iaqi;
+        const { aqi, dominentpol, iaqi, city } = data.data;
+        const { t, h, p, pm10, } = iaqi;
         document.getElementById('status').innerHTML = `
         <h2><strong>Current Status:</strong> ${getAqiCategory(aqi)}</h2>
     `;
@@ -28,25 +43,99 @@ function parseData(data){
         `;
         document.getElementById('dominantPollutant').innerHTML = `
         <p><strong>Dominant Pollutant:</strong> ${dominentpol.toUpperCase()}</p>
+        <br>
+        <p><strong>PM10:</strong> ${pm10 ? pm10.v : 'N/A'} µg/m³</p>
+
     `;
-    document.getElementById('footer').innerHTML = `
-    <p><strong>Last updated at:</strong> ${Date()}. Feel free to interact with this display! </p>
-`;
-    /*document.getElementById('waqiData').innerHTML = `
-    <p><strong>Air Quality Index in ${city.name}:</strong> ${aqi} </p>
-    <p><strong>Dominant Pollutant:</strong> ${dominentpol.toUpperCase()}</p>
-    <p><strong>Current Temperature:</strong> ${t ? t.v : 'N/A'}°C</p>
-    <p><strong>Current Humidity:</strong> ${h ? h.v : 'N/A'}%</p>
-    <p><strong>Current Pressure:</strong> ${p ? p.v : 'N/A'} hPa</p>
-    <p><strong>Current PM10:</strong> ${pm10 ? pm10.v : 'N/A'} µg/m³</p>
-    <p><strong>Last updated at:</strong> ${Date()}</p>
-`;*/
+        document.getElementById('footer').innerHTML = `
+    <p><strong>Last updated at:</strong> ${Date()}. All Data is from the World Air Quality Index Project. </p>
+    `;
     } else {
         document.getElementById('status').innerText = 'No data available.';
     }
-    
+
 }
 
+//This function should be used if the user is on the extended data page. This function will setup graphs and the status bar, and update them with data.
+function parseExtended(data) {
+    if (data.status === "ok") {
+        const { aqi, dominentpol, iaqi, city } = data.data;
+        const { t, h, p, pm10, } = iaqi;   
+        
+        document.getElementById('status').innerHTML = `
+        <h2><strong>Current Status:</strong> ${getAqiCategory(aqi)}</h2>
+        `;
+        document.getElementById('footer').innerHTML = `
+        <p><strong>Last updated at:</strong> ${Date()}. All Data is from the World Air Quality Index Project. </p>
+        `;
+        
+
+    // If the AQI chart isn't made, create the chart
+    if (!aqiChart) {
+        aqiChart = Highcharts.chart('aqiGraph', {
+            chart: {
+                type: 'histogram',
+                height: 100 +'%',  
+            },
+            title: {
+                text: 'AQI'
+            },
+            xAxis: {
+                title: {
+                    text: 'Times Updated'
+                }
+            },
+            yAxis: {
+                title: {
+                    text: 'AQI'
+                }
+            },
+            series: [{
+                type: 'histogram',
+            }]
+        });
+        //Update the chart with some Data
+        aqiChart.series[0].update({name:"Air Quality Index"}, false);
+    }
+    // If the PM10 chart isn't made, create the chart
+    if (!pm10Chart) {
+        pm10Chart = Highcharts.chart('pm10Graph', {
+            chart: {
+                type: 'histogram',
+                height: 100 +'%',  
+            },
+            title: {
+                text: 'PM10'
+            },
+            xAxis: {
+                title: {
+                    text: 'Times Updated'
+                }
+            },
+            yAxis: {
+                title: {
+                    text: 'PM10 µg/m³'
+                }
+            },
+            series: [{
+                type: 'histogram',
+            }]
+        });
+        //Update the chart with some Data
+        pm10Chart.series[0].update({name:"PM10 µg/m³"}, false);
+    }
+
+    pm10Chart.series[0].addPoint(pm10 ? pm10.v : 'N/A');
+
+    aqiChart.series[0].addPoint(aqi);
+    
+    } else {
+        document.getElementById('status').innerText = 'No data available.';
+    }
+
+}
+
+// This function determines the colouring of the interface.
 function getAqiCategory(aqi) {
     let category;
     let color;
@@ -75,44 +164,11 @@ function getAqiCategory(aqi) {
     const dataDivs = document.querySelectorAll('.data');
     dataDivs.forEach(div => {
         div.style.backgroundColor = color;
+        
     });
 
     return category;
 }
 
- ////Token: f3dd92ec3861361466172655ca608938c634dddc
 
-
-/*
-function getAqiCategory(aqi) {
-
-    if (aqi <= 50) return 'Good';
-    if (aqi <= 100) return 'Moderate';
-    if (aqi <= 150) return 'Unhealthy for Sensitive Groups';
-    if (aqi <= 200) return 'Unhealthy';
-    if (aqi <= 300) return 'Very Unhealthy';
-    return 'Hazardous';
-}
-*/
-
-/*function drawChart() {
-    google.charts.load('current', {packages: ['corechart']});
-    google.charts.setOnLoadCallback(drawChart);
-  // Define the chart to be drawn.
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Element');
-  data.addColumn('number', 'Percentage');
-  data.addRows([
-    ['Nitrogen', 0.78],
-    ['Oxygen', 0.21],
-    ['Other', 0.01]
-  ]);
-
-   // Instantiate and draw the chart.
-   var chart = new google.visualization.PieChart(document.getElementById('graphs'));
-  chart.draw(data, null);
-}
-
-
-window.onload = drawChart();*/
 
