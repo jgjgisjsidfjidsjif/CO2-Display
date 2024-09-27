6ae347917b51e96270211c1c60aa991994626050
jgjgisjsidfjidsjif <126114030+jgjgisjsidfjidsjif@users.noreply.github.com>
2024-09-27 22:19:27 +1200

    Version 3
    Latest version. The token in the script.js file is redacted for obvious reasons. This version has all of the improvements requested on the review section of Version 2 completed, like fixing formatting errors, HTML/CSS convention and accessibility checks being satisfied, as well as improving the interactability, now including a call of action and an about page. More details are seen in the test log excel.

--- script.js
+++ script.js
@@ -16,15 +16,16 @@ async function getData() { //This retrives data from the WAQI API for Pakuranga.
         }
         const data = await response.json();
         //This is where the magic happens. This if else statement determines which page the user is on, and determines which function should be used.
-        if (document.getElementById('extendedStatistics')){
+        if (document.getElementById('extendedStatistics')) {
             parseData(data);
-            console.log("This is the index page.");
         }
-        else{
+        else if (document.getElementById('aqiGraph')) {
             parseExtended(data);
-            console.log("This is NOT!!! the index page.");
+        }
+        else {
+            parseMinimal(data);
         };
-         
+
     } catch (error) {
         console.error(error.message);
     }
@@ -37,13 +38,15 @@ function parseData(data) {
         const { t, h, p, pm10, } = iaqi;
         document.getElementById('status').innerHTML = `
         <h2><strong>Current Status:</strong> ${getAqiCategory(aqi)}</h2>
+        <hr>
+        <a href="./about.html"><h3>Learn more!</h3></a>
     `;
         document.getElementById('currentIndex').innerHTML = `
             <p><strong>Air Quality Index in ${city.name}:</strong> ${aqi} </p>
         `;
         document.getElementById('dominantPollutant').innerHTML = `
         <p><strong>Dominant Pollutant:</strong> ${dominentpol.toUpperCase()}</p>
-        <br>
+        <hr>
         <p><strong>PM10:</strong> ${pm10 ? pm10.v : 'N/A'} µg/m³</p>
 
     `;
@@ -56,79 +59,99 @@ function parseData(data) {
 
 }
 
+//This function is called for the about page. Only the status header and footer get updated.
+function parseMinimal(data) {
+    if (data.status === "ok") {
+        const { aqi, dominentpol, iaqi, city } = data.data;
+        const { t, h, p, pm10, } = iaqi;
+        document.getElementById('status').innerHTML = `
+        <h2><strong>Current Status:</strong> ${getAqiCategory(aqi)}</h2>
+        <hr>
+        <a href="./"><h3>Back to the homepage</h3></a>
+        `;
+        document.getElementById('footer').innerHTML = `
+    <p><strong>Last updated at:</strong> ${Date()}. All Data is from the World Air Quality Index Project. </p>
+    `;
+    } else {
+        document.getElementById('status').innerText = 'No data available.';
+    }
+}
+
 //This function should be used if the user is on the extended data page. This function will setup graphs and the status bar, and update them with data.
 function parseExtended(data) {
     if (data.status === "ok") {
         const { aqi, dominentpol, iaqi, city } = data.data;
-        const { t, h, p, pm10, } = iaqi;   
-        
+        const { t, h, p, pm10, } = iaqi;
+
         document.getElementById('status').innerHTML = `
         <h2><strong>Current Status:</strong> ${getAqiCategory(aqi)}</h2>
+        <hr>
+        <a href="./"><h3>Back to the homepage</h3></a>
         `;
         document.getElementById('footer').innerHTML = `
         <p><strong>Last updated at:</strong> ${Date()}. All Data is from the World Air Quality Index Project. </p>
         `;
-        
 
-    // If the AQI chart isn't made, create the chart
-    if (!aqiChart) {
-        aqiChart = Highcharts.chart('aqiGraph', {
-            chart: {
-                type: 'histogram',
-                height: 100 +'%',  
-            },
-            title: {
-                text: 'AQI'
-            },
-            xAxis: {
-                title: {
-                    text: 'Times Updated'
-                }
-            },
-            yAxis: {
+
+        // If the AQI chart isn't made, create the chart
+        if (!aqiChart) {
+            aqiChart = Highcharts.chart('aqiGraph', {
+                chart: {
+                    type: 'histogram',
+                    height: 100 + '%',
+                },
                 title: {
                     text: 'AQI'
-                }
-            },
-            series: [{
-                type: 'histogram',
-            }]
-        });
-        //Update the chart with some Data
-        aqiChart.series[0].update({name:"Air Quality Index"}, false);
-    }
-    // If the PM10 chart isn't made, create the chart
-    if (!pm10Chart) {
-        pm10Chart = Highcharts.chart('pm10Graph', {
-            chart: {
-                type: 'histogram',
-                height: 100 +'%',  
-            },
-            title: {
-                text: 'PM10'
-            },
-            xAxis: {
+                },
+                xAxis: {
+                    title: {
+                        text: 'Times Updated'
+                    }
+                },
+                yAxis: {
+                    title: {
+                        text: 'AQI'
+                    }
+                },
+                series: [{
+                    type: 'histogram',
+                }]
+            });
+            //Update the chart with some Data
+            aqiChart.series[0].update({ name: "Air Quality Index" }, false);
+        }
+        // If the PM10 chart isn't made, create the chart
+        if (!pm10Chart) {
+            pm10Chart = Highcharts.chart('pm10Graph', {
+                chart: {
+                    type: 'histogram',
+                    height: 100 + '%',
+                },
                 title: {
-                    text: 'Times Updated'
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'PM10 µg/m³'
-                }
-            },
-            series: [{
-                type: 'histogram',
-            }]
-        });
-        //Update the chart with some Data
-        pm10Chart.series[0].update({name:"PM10 µg/m³"}, false);
-    }
+                    text: 'PM10'
+                },
+                xAxis: {
+                    title: {
+                        text: 'Times Updated'
+                    }
+                },
+                yAxis: {
+                    title: {
+                        text: 'PM10 µg/m³'
+                    }
+                },
+                series: [{
+                    type: 'histogram',
+                }]
+            });
+            //Update the chart with some Data
+            pm10Chart.series[0].update({ name: "PM10 µg/m³" }, false);
+        }
 
-    pm10Chart.series[0].addPoint(pm10 ? pm10.v : 'N/A');
+        pm10Chart.series[0].addPoint(pm10 ? pm10.v : 'N/A');
 
-    aqiChart.series[0].addPoint(aqi);
-    
+        aqiChart.series[0].addPoint(aqi);
+
     } else {
         document.getElementById('status').innerText = 'No data available.';
     }
@@ -164,7 +187,7 @@ function getAqiCategory(aqi) {
     const dataDivs = document.querySelectorAll('.data');
     dataDivs.forEach(div => {
         div.style.backgroundColor = color;
-        
+
     });
 
     return category;
